82ccef975cda8923f4c5c6ef938c79cd11f130ac
Vsevolod Kolobov <vsevolok.atribut@gmail.com>
2020-02-02 22:05:04 +0300

    Initialized by Spring Boot

---
+++ src/main/java/net/vsevolod/chat/MainApplication.java
@@ -0,0 +1,20 @@
+package net.vsevolod.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Vsevolod Kolobov
+ */
+
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(net.vsevolod.chat.MainApplication.class, args);
+    }
+
+}
+
+
