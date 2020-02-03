f82deb44e5c1cf2332424b7b78ab3a361f568f0f
Vsevolod Kolobov <vsevolok.atribut@gmail.com>
2020-02-03 23:50:59 +0300

    Created a websocket connection between clients

---
+++ src/main/java/net/vsevolod/chat/controller/WebSocketController.java
@@ -0,0 +1,31 @@
+package net.vsevolod.chat.controller;
+
+import net.vsevolod.chat.model.ChatMessage;
+import org.springframework.stereotype.Controller;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Vsevolod Kolobov
+ */
+
+@Controller
+public class WebSocketController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/publicChatRoom")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/publicChatRoom")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+}
---
+++ src/main/java/net/vsevolod/chat/websocket/listener/WebSocketEventListener.java
@@ -0,0 +1,39 @@
+package net.vsevolod.chat.websocket.listener;
+
+import net.vsevolod.chat.model.ChatMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+/**
+ * @author Vsevolod Kolobov
+ */
+public class WebSocketEventListener {
+
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+
+        if (username != null) {
+
+            ChatMessage chatMessage = new ChatMessage();
+            chatMessage.setType(ChatMessage.MessageType.LEAVE);
+            chatMessage.setSender(username);
+
+            messagingTemplate.convertAndSend("/topic/publicChatRoom", chatMessage);
+        }
+    }
+
+}
